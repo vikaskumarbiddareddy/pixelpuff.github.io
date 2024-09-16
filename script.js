28df7f473dbf4eb3827b82ce026a4ee2adff833a
vikaskumarbiddareddy <vikaskumar.biddareddy@gmail.com>
2024-09-16 12:52:00 +0530

    upload files

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // JavaScript for handling button click
+    document.getElementById('learnMoreBtn').addEventListener('click', function() {
+        alert('Stay tuned for more information about Pixelpuff!');
+    });
+});
